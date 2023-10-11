4a28f996233f7e03c6ec7e3535508453f9490052
Marc Nuri <marc@marcnuri.com>
2023-10-11 07:35:46 +0200

    feat: CORS support in Go

--- go/uuid_resource.go
+++ go/uuid_resource.go
@@ -2,21 +2,40 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"os"
 	"strconv"
+	"strings"
 )
 
 const bulkMin = 1
 const bulkMax = 20000
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Error: ", r)
+			os.Exit(1)
+		}
+	}()
 	gin.SetMode(gin.ReleaseMode)
 	router := setupRouter()
-	router.Run("0.0.0.0:8080")
+	err := router.Run("0.0.0.0:8080")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
+	allowedOrigins := os.Getenv("ALLOWED_ORIGINS")
+	if len(allowedOrigins) != 0 {
+		config := cors.DefaultConfig()
+		config.AllowOrigins = strings.Split(allowedOrigins, ",")
+		router.Use(cors.New(config))
+	}
 	router.GET("/", get)
 	return router
 }
--- go/uuid_resource_test.go
+++ go/uuid_resource_test.go
@@ -7,64 +7,100 @@ import (
 	"testing"
 )
 
-var router *gin.Engine
+type testContext struct {
+	router   *gin.Engine
+	recorder *httptest.ResponseRecorder
+}
 
-func TestMain(m *testing.M) {
-	router = setupRouter()
-	m.Run()
+func (c *testContext) beforeEach() {
+	c.router = setupRouter()
+	c.recorder = httptest.NewRecorder()
+}
+
+func (c *testContext) afterEach() {
+}
+
+func testCase(test func(t *testing.T, c *testContext)) func(*testing.T) {
+	return func(t *testing.T) {
+		context := &testContext{}
+		context.beforeEach()
+		defer context.afterEach()
+		test(t, context)
+	}
 }
 
 func TestNoQuantityParam(t *testing.T) {
-	t.Run("Returns 200 status code", func(t *testing.T) {
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, httptest.NewRequest("GET", "/", nil))
-		if recorder.Code != 200 {
-			t.Error("Expected 200, got ", recorder.Code)
+	t.Run("Returns 200 status code", testCase(func(t *testing.T, c *testContext) {
+		c.router.ServeHTTP(c.recorder, httptest.NewRequest("GET", "/", nil))
+		if c.recorder.Code != 200 {
+			t.Error("Expected 200, got ", c.recorder.Code)
 		}
-	})
-	t.Run("Returns a single UUID", func(t *testing.T) {
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, httptest.NewRequest("GET", "/", nil))
-		body := recorder.Body.String()
+	}))
+	t.Run("Returns a single UUID", testCase(func(t *testing.T, c *testContext) {
+		c.router.ServeHTTP(c.recorder, httptest.NewRequest("GET", "/", nil))
+		body := c.recorder.Body.String()
 		isUuid, _ := regexp.MatchString("^\\[\\n\\s+\"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\"\\n]$", body)
 		if !isUuid {
 			t.Error("Returned body doesn't contain a UUID: ", body)
 		}
-	})
+	}))
 }
 
 func TestInvalidQuantityParam(t *testing.T) {
-	t.Run("Returns 400 status code", func(t *testing.T) {
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, httptest.NewRequest("GET", "/?quantity=not-a-number", nil))
-		if recorder.Code != 400 {
-			t.Error("Expected 400, got ", recorder.Code)
+	t.Run("Returns 400 status code", testCase(func(t *testing.T, c *testContext) {
+		c.router.ServeHTTP(c.recorder, httptest.NewRequest("GET", "/?quantity=not-a-number", nil))
+		if c.recorder.Code != 400 {
+			t.Error("Expected 400, got ", c.recorder.Code)
 		}
-	})
-	t.Run("Returns body with message", func(t *testing.T) {
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, httptest.NewRequest("GET", "/?quantity=not-a-number", nil))
-		body := recorder.Body.String()
+	}))
+	t.Run("Returns body with message", testCase(func(t *testing.T, c *testContext) {
+		c.router.ServeHTTP(c.recorder, httptest.NewRequest("GET", "/?quantity=not-a-number", nil))
+		body := c.recorder.Body.String()
 		if body != "Invalid quantity not-a-number" {
 			t.Error("Returned body doesn't contain expected message: ", body)
 		}
-	})
+	}))
 }
 
 func TestOutOfBoundsQuantityParam(t *testing.T) {
-	t.Run("Returns 400 status code", func(t *testing.T) {
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, httptest.NewRequest("GET", "/?quantity=99999999", nil))
-		if recorder.Code != 400 {
-			t.Error("Expected 400, got ", recorder.Code)
+	t.Run("Returns 400 status code", testCase(func(t *testing.T, c *testContext) {
+		c.router.ServeHTTP(c.recorder, httptest.NewRequest("GET", "/?quantity=99999999", nil))
+		if c.recorder.Code != 400 {
+			t.Error("Expected 400, got ", c.recorder.Code)
 		}
-	})
-	t.Run("Returns body with message", func(t *testing.T) {
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, httptest.NewRequest("GET", "/?quantity=-100", nil))
-		body := recorder.Body.String()
+	}))
+	t.Run("Returns body with message", testCase(func(t *testing.T, c *testContext) {
+		c.router.ServeHTTP(c.recorder, httptest.NewRequest("GET", "/?quantity=-100", nil))
+		body := c.recorder.Body.String()
 		if body != "Quantity must be between 1 and 20000" {
 			t.Error("Returned body doesn't contain expected message: ", body)
 		}
-	})
+	}))
+}
+
+func TestCors(t *testing.T) {
+	t.Setenv("ALLOWED_ORIGINS", "https://example.com")
+	t.Run("No CORS applied when request has no origin", testCase(func(t *testing.T, c *testContext) {
+		c.router.ServeHTTP(c.recorder, httptest.NewRequest("GET", "/", nil))
+		if c.recorder.Code != 200 {
+			t.Error("Expected 200, got ", c.recorder.Code)
+		}
+	}))
+	t.Run("Returns Access-Control-Allow-Origin header when CORS request", testCase(func(t *testing.T, c *testContext) {
+		request := httptest.NewRequest("GET", "/", nil)
+		request.Host = "server1.example.com"
+		request.Header.Add("Origin", "https://example.com")
+		c.router.ServeHTTP(c.recorder, request)
+		if c.recorder.Header().Get("Access-Control-Allow-Origin") != "https://example.com" {
+			t.Error("Expected Access-Control-Allow-Origin header, got", c.recorder.Header().Get("Access-Control-Allow-Origin"))
+		}
+	}))
+	t.Run("Returns 403 Forbidden status code when request has origin", testCase(func(t *testing.T, c *testContext) {
+		request := httptest.NewRequest("GET", "/", nil)
+		request.Header.Add("Origin", "https://different.com")
+		c.router.ServeHTTP(c.recorder, request)
+		if c.recorder.Code != 403 {
+			t.Error("Expected 403, got ", c.recorder.Code)
+		}
+	}))
 }
